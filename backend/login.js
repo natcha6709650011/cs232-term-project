c73fa3af1f91e36759e5eef3be8574ed42fa8353
Your Name <natcha.kanch@dome.tu.ac.th>
2026-05-08 00:06:05 +0700

    edit role

--- backend/login.js
+++ backend/login.js
@@ -123,8 +123,7 @@ let userType = tuData.type || "student";
 // DEMO: บังคับ account กลุ่มนี้ให้เป็นอาจารย์
 const DEMO_TEACHER_USERNAMES = [
   "6709650029",
-  "6709650011",
-  "6709650250"
+  "6709650011"
 ];
 
 if (DEMO_TEACHER_USERNAMES.includes(tuData.username)) {
